a3546157e7d509b3ac83306237a5fd1eaa03ecab
marlex <marlex.ladag@gettonline.com>
2021-06-18 19:36:47 +0800

    fix: import validation and db

--- src/Service/CarrierConfigurationProvider.php
+++ src/Service/CarrierConfigurationProvider.php
@@ -2,6 +2,9 @@
 
 namespace Gett\MyparcelBE\Service;
 
+use Db;
+use Validate;
+
 class CarrierConfigurationProvider
 {
     public static $configuration;
@@ -9,7 +12,7 @@ class CarrierConfigurationProvider
     public static function get(int $carrier_id, string $name, $default = null)
     {
         if (!isset(static::$configuration[$carrier_id][$name])) {
-            $result = \Db::getInstance()->executeS('SELECT name,value FROM ' . _DB_PREFIX_ . "myparcelbe_carrier_configuration WHERE id_carrier = '{$carrier_id}' ");
+            $result = Db::getInstance()->executeS('SELECT name,value FROM ' . _DB_PREFIX_ . "myparcelbe_carrier_configuration WHERE id_carrier = '{$carrier_id}' ");
 
             foreach ($result as $item) {
                 static::$configuration[$carrier_id][$item['name']] = $item['value'];
@@ -21,10 +24,12 @@ class CarrierConfigurationProvider
 
     public static function updateValue(int $carrier_id, string $name, string $value)
     {
-        \Db::getInstance()->update(
-            'myparcelbe_carrier_configuration',
-            ['value' => pSQL($value)],
-            'id_carrier = ' . (int) $carrier_id . ' AND name = "' . pSQL($name) . '" '
-        );
+        if (Validate::isConfigName($name)) {
+            Db::getInstance()->update(
+                'myparcelbe_carrier_configuration',
+                ['value' => pSQL($value)],
+                'id_carrier = ' . (int) $carrier_id . ' AND name = "' . pSQL($name) . '" '
+            );
+        }
     }
 }
