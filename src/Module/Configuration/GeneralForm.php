5d9e711b7acfcc07e39759d534992ca579e61342
Edie Lemoine <edie@myparcel.nl>
2021-09-28 17:21:11 +0200

    feat: add surcharge option to delivery options

--- src/Module/Configuration/GeneralForm.php
+++ src/Module/Configuration/GeneralForm.php
@@ -1,78 +1,61 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Gett\MyparcelBE\Module\Configuration;
 
 use Gett\MyparcelBE\Constant;
 
 class GeneralForm extends AbstractForm
 {
-    protected $icon = 'cog';
-
-    protected function getLegend(): string
+    protected function getNamespace(): string
     {
-        return $this->module->l('General Settings', 'generalform');
+        return 'generalform';
     }
 
+    /**
+     * @return array[]
+     */
     protected function getFields(): array
     {
         return [
-            Constant::SHARE_CUSTOMER_EMAIL_CONFIGURATION_NAME => [
-                'type' => 'switch',
-                'label' => $this->module->l('Share customer email with MyParcel', 'generalform'),
-                'name' => Constant::SHARE_CUSTOMER_EMAIL_CONFIGURATION_NAME,
-                'required' => false,
-                'is_bool' => true,
-                'values' => [
-                    [
-                        'id' => 'active_on',
-                        'value' => 1,
-                        'label' => $this->module->l('Enabled', 'generalform'),
-                    ],
-                    [
-                        'id' => 'active_off',
-                        'value' => 0,
-                        'label' => $this->module->l('Disabled', 'generalform'),
-                    ],
-                ],
+            Constant::SHARE_CUSTOMER_EMAIL_CONFIGURATION_NAME          => [
+                'type'     => self::FIELD_TYPE_SWITCH,
+                'label'    => $this->module->l('Share customer email with MyParcel', $this->getNamespace()),
+                'name'     => Constant::SHARE_CUSTOMER_EMAIL_CONFIGURATION_NAME,
             ],
-            Constant::SHARE_CUSTOMER_PHONE_CONFIGURATION_NAME => [
-                'type' => 'switch',
-                'label' => $this->module->l('Share customer phone with MyParcel', 'generalform'),
-                'name' => Constant::SHARE_CUSTOMER_PHONE_CONFIGURATION_NAME,
-                'required' => false,
-                'is_bool' => true,
-                'values' => [
-                    [
-                        'id' => 'active_on',
-                        'value' => 1,
-                        'label' => $this->module->l('Enabled', 'generalform'),
-                    ],
-                    [
-                        'id' => 'active_off',
-                        'value' => 0,
-                        'label' => $this->module->l('Disabled', 'generalform'),
-                    ],
-                ],
+            Constant::SHARE_CUSTOMER_PHONE_CONFIGURATION_NAME          => [
+                'type'     => self::FIELD_TYPE_SWITCH,
+                'label'    => $this->module->l('Share customer phone with MyParcel', $this->getNamespace()),
+                'name'     => Constant::SHARE_CUSTOMER_PHONE_CONFIGURATION_NAME,
             ],
             Constant::USE_ADDRESS2_AS_STREET_NUMBER_CONFIGURATION_NAME => [
-                'type' => 'switch',
-                'label' => $this->module->l('Use second address field in checkout as street number', 'generalform'),
-                'name' => Constant::USE_ADDRESS2_AS_STREET_NUMBER_CONFIGURATION_NAME,
-                'required' => false,
-                'is_bool' => true,
-                'values' => [
-                    [
-                        'id' => 'active_on',
-                        'value' => 1,
-                        'label' => $this->module->l('Enabled', 'generalform'),
-                    ],
-                    [
-                        'id' => 'active_off',
-                        'value' => 0,
-                        'label' => $this->module->l('Disabled', 'generalform'),
+                'type'     => self::FIELD_TYPE_SWITCH,
+                'label'    => $this->module->l('Use second address field in checkout as street number', $this->getNamespace()),
+                'name'     => Constant::USE_ADDRESS2_AS_STREET_NUMBER_CONFIGURATION_NAME,
+            ],
+            Constant::DELIVERY_OPTIONS_PRICE_FORMAT_CONFIGURATION_NAME => [
+                'type'    => self::FIELD_TYPE_SELECT,
+                'label'   => $this->module->l('Show prices as', $this->getNamespace()),
+                'name'    => Constant::DELIVERY_OPTIONS_PRICE_FORMAT_CONFIGURATION_NAME,
+                'options' => [
+                    'query' => [
+                        [
+                            'id'   => Constant::DELIVERY_OPTIONS_PRICE_FORMAT_TOTAL_PRICE,
+                            'name' => $this->module->l('Total price', $this->getNamespace()),
+                        ],
+                        [
+                            'id'   => Constant::DELIVERY_OPTIONS_PRICE_FORMAT_SURCHARGE,
+                            'name' => $this->module->l('Surcharge', $this->getNamespace()),
+                        ],
                     ],
                 ],
             ],
         ];
+    }
+
+    protected function getLegend(): string
+    {
+        return $this->module->l('General Settings', $this->getNamespace());
     }
 }
