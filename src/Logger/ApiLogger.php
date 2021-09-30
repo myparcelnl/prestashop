954bc187360b1fa96f3385bd05e085bd45c8a9db
Edie Lemoine <edie@myparcel.nl>
2021-09-30 14:38:43 +0200

    fix: fix status change webhook not triggering sometimes

--- src/Logger/ApiLogger.php
+++ src/Logger/ApiLogger.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Gett\MyparcelBE\Logger;
 
 use Configuration;
@@ -8,24 +10,23 @@ use Gett\MyparcelBE\Constant;
 /**
  * Only logs data if API Logging is enabled in the MyParcel settings.
  */
-class ApiLogger extends Logger
+class ApiLogger extends FileLogger
 {
     /**
-     * @param        $message
-     * @param  bool  $isException
-     * @param  false $allowDuplicate
-     * @param  int   $severity
-     * @param  null  $errorCode
+     * @param  mixed $message
+     * @param  int   $level
      */
     public static function addLog(
-             $message,
-        bool $isException = false,
-        bool $allowDuplicate = false,
-        int  $severity = 1,
-             $errorCode = null
-    ) {
-        if (Configuration::get(Constant::API_LOGGING_CONFIGURATION_NAME)) {
-            parent::addLog($message, $isException, $allowDuplicate, $severity, $errorCode);
+        $message,
+        int $level = self::DEBUG
+    ): void {
+        if (! Configuration::get(Constant::API_LOGGING_CONFIGURATION_NAME)) {
+            return;
         }
+
+        parent::addLog($message, $level);
+
+        // Also log to the DB for the time being.
+        Logger::addLog($message, $level > 0, true, $level);
     }
 }
