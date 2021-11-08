ee147f628a3b52cd6d57f060fa1ec93988bf1ab3
Joeri van Veen <joeri@myparcel.nl>
2021-11-08 23:26:23 +0100

    fix: auto-translate older carrier ids to the current ones

--- src/Service/CarrierConfigurationProvider.php
+++ src/Service/CarrierConfigurationProvider.php
@@ -14,6 +14,11 @@ class CarrierConfigurationProvider
     private static $configuration;
 
     /**
+     * @var array
+     */
+    private static $legacyCarrierIdMap;
+
+    /**
      * @var string
      */
     private static $table = Table::TABLE_CARRIER_CONFIGURATION;
@@ -28,11 +33,12 @@ class CarrierConfigurationProvider
      */
     public static function get(int $carrierId, string $name, $default = null)
     {
+        self::fetchLegacyCarrierIdMap();
         if (! isset(static::$configuration[$carrierId][$name])) {
             $query = (new DbQuery())
                 ->select('name, value')
                 ->from(self::$table)
-                ->where('id_carrier = ' . $carrierId);
+                ->where('id_carrier = ' . static::$legacyCarrierIdMap[$carrierId]);
 
             $result = Db::getInstance()
                 ->executeS($query);
@@ -60,5 +66,27 @@ class CarrierConfigurationProvider
                 ['value' => pSQL($value)],
                 'id_carrier = ' . $carrierId . ' AND name = "' . pSQL($name) . '" '
             );
+    }
+
+    /**
+     * @throws \PrestaShopDatabaseException
+     */
+    private static function fetchLegacyCarrierIdMap(): void
+    {
+        if (! isset(static::$legacyCarrierIdMap)) {
+            $query = new DbQuery();
+            $query->select('current.id_carrier as current_carrier_id, old.id_carrier as old_carrier_id');
+            $query->from(Table::withPrefix('carrier'), 'current');
+            $query->innerJoin(Table::withPrefix('carrier'), 'old', 'old.name = current.name');
+            $query->where('current.active=1');
+            $query->where('current.deleted=0');
+            $query->orderBy('current.id_carrier DESC');
+
+            $rows = Db::getInstance(_PS_USE_SQL_SLAVE_)
+                ->executeS($query);
+            foreach ($rows as $row) {
+                static::$legacyCarrierIdMap[$row['old_carrier_id']] = $row['current_carrier_id'];
+            }
+        }
     }
 }
