a9da537992965d7a061ecce8d52fded3f8c663c2
Richard Perdaan <richard@myparcel.nl>
2021-11-11 17:21:35 +0100

    fix: update carrier configuration table in a new upgrade file

---
+++ upgrade/upgrade-1.4.0.php
@@ -0,0 +1,20 @@
+<?php
+
+use Gett\MyparcelBE\Constant;
+use Gett\MyparcelBE\Database\Table;
+
+/**
+ * @throws \PrestaShopDatabaseException
+ */
+function upgrade_module_1_4_0(MyParcelBE $module): bool
+{
+    $carrierConfigurationTable = Table::withPrefix(Table::TABLE_CARRIER_CONFIGURATION);
+
+    $query = <<<SQL
+ALTER TABLE $carrierConfigurationTable ALTER COLUMN type text NOT NULL;
+SQL;
+
+    Db::getInstance()->execute($query);
+
+    return true;
+}
