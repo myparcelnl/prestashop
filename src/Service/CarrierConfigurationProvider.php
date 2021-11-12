5f9a037f8b83c2681e14f8ea77eb23f4bb506511
Joeri van Veen <nonstockphoto@gmail.com>
2021-11-12 11:48:59 +0100

    fix: auto-translate older carrier ids to the current ones (#60)

--- src/Service/CarrierConfigurationProvider.php
+++ src/Service/CarrierConfigurationProvider.php
@@ -14,25 +14,30 @@ class CarrierConfigurationProvider
     private static $configuration;
 
     /**
+     * @var array
+     */
+    private static $legacyCarrierIdMap;
+
+    /**
      * @var string
      */
     private static $table = Table::TABLE_CARRIER_CONFIGURATION;
 
     /**
-     * @param  int    $carrierId
-     * @param  string $name
-     * @param  null   $default
+     * @param int    $carrierId
+     * @param string $name
+     * @param null   $default
      *
      * @return null|mixed
      * @throws \PrestaShopDatabaseException
      */
     public static function get(int $carrierId, string $name, $default = null)
     {
+        self::fetchLegacyCarrierIdMap();
         if (! isset(static::$configuration[$carrierId][$name])) {
-            $query = (new DbQuery())
-                ->select('name, value')
+            $query = (new DbQuery())->select('name, value')
                 ->from(self::$table)
-                ->where('id_carrier = ' . $carrierId);
+                ->where('id_carrier = ' . (static::$legacyCarrierIdMap[$carrierId] ?? 0));
 
             $result = Db::getInstance()
                 ->executeS($query);
@@ -48,9 +53,9 @@ class CarrierConfigurationProvider
     }
 
     /**
-     * @param  int    $carrierId
-     * @param  string $name
-     * @param  string $value
+     * @param int    $carrierId
+     * @param string $name
+     * @param string $value
      */
     public static function updateValue(int $carrierId, string $name, string $value): void
     {
@@ -60,5 +65,29 @@ class CarrierConfigurationProvider
                 ['value' => pSQL($value)],
                 'id_carrier = ' . $carrierId . ' AND name = "' . pSQL($name) . '" '
             );
+    }
+
+    /**
+     * @throws \PrestaShopDatabaseException
+     */
+    private static function fetchLegacyCarrierIdMap(): void
+    {
+        if (isset(static::$legacyCarrierIdMap)) {
+            return;
+        }
+
+        $query = new DbQuery();
+        $query->select('current.id_carrier as current_carrier_id, old.id_carrier as old_carrier_id');
+        $query->from(Table::withPrefix('carrier'), 'current');
+        $query->innerJoin(Table::withPrefix('carrier'), 'old', 'old.name = current.name');
+        $query->where('current.active=1');
+        $query->where('current.deleted=0');
+        $query->orderBy('current.id_carrier DESC');
+
+        $rows = Db::getInstance(_PS_USE_SQL_SLAVE_)
+            ->executeS($query);
+        foreach ($rows as $row) {
+            static::$legacyCarrierIdMap[$row['old_carrier_id']] = $row['current_carrier_id'];
+        }
     }
 }
