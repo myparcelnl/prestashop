ef22aa880801ee76f9b1f714662209f02bebc692
markernst97 <markiervc@gmail.com>
2022-05-04 16:19:38 +0200

    feat: add more translations for checkout

--- src/Module/Configuration/Form/CheckoutForm.php
+++ src/Module/Configuration/Form/CheckoutForm.php
@@ -6,27 +6,33 @@ namespace Gett\MyparcelBE\Module\Configuration\Form;
 
 class CheckoutForm extends AbstractForm
 {
-    public const  CONFIGURATION_ADDRESS_NOT_FOUND       = 'MYPARCELBE_ADDRESS_NOT_FOUND_TITLE';
-    public const  CONFIGURATION_CITY                    = 'MYPARCELBE_CITY_TITLE';
-    public const  CONFIGURATION_CLOSED                  = 'MYPARCELBE_CLOSED_TITLE';
-    public const  CONFIGURATION_DELIVERY_EVENING_TITLE  = 'MYPARCELBE_DELIVERY_EVENING_TITLE';
-    public const  CONFIGURATION_DELIVERY_MORNING_TITLE  = 'MYPARCELBE_DELIVERY_MORNING_TITLE';
-    public const  CONFIGURATION_DELIVERY_STANDARD_TITLE = 'MYPARCELBE_DELIVERY_STANDARD_TITLE';
-    public const  CONFIGURATION_DELIVERY_TITLE          = 'MYPARCELBE_DELIVERY_TITLE';
-    public const  CONFIGURATION_DISCOUNT                = 'MYPARCELBE_DISCOUNT_TITLE';
-    public const  CONFIGURATION_FREE                    = 'MYPARCELBE_FREE_TITLE';
-    public const  CONFIGURATION_FROM                    = 'MYPARCELBE_FROM_TITLE';
-    public const  CONFIGURATION_HOUSE_NUMBER            = 'MYPARCELBE_HOUSE_NUMBER_TITLE';
-    public const  CONFIGURATION_LOAD_MORE               = 'MYPARCELBE_LOAD_MORE_TITLE';
-    public const  CONFIGURATION_ONLY_RECIPIENT_TITLE    = 'MYPARCELBE_ONLY_RECIPIENT_TITLE';
-    public const  CONFIGURATION_OPENING_HOURS           = 'MYPARCELBE_OPENING_HOURS_TITLE';
-    public const  CONFIGURATION_PICKUP_TITLE            = 'MYPARCELBE_PICKUP_TITLE';
-    public const  CONFIGURATION_PICK_UP_FROM            = 'MYPARCELBE_PICK_UP_FROM_TITLE';
-    public const  CONFIGURATION_POSTCODE                = 'MYPARCELBE_POSTCODE_TITLE';
-    public const  CONFIGURATION_RETRY                   = 'MYPARCELBE_RETRY_TITLE';
-    public const  CONFIGURATION_SATURDAY_DELIVERY_TITLE = 'MYPARCELBE_SATURDAY_DELIVERY_TITLE';
-    public const  CONFIGURATION_SIGNATURE_TITLE         = 'MYPARCELBE_SIGNATURE_TITLE';
-    public const  CONFIGURATION_WRONG_POSTAL_CODE_CITY  = 'MYPARCELBE_WRONG_POSTAL_CODE_CITY_TITLE';
+    public const  CONFIGURATION_ADDRESS_NOT_FOUND        = 'MYPARCELBE_ADDRESS_NOT_FOUND_TITLE';
+    public const  CONFIGURATION_CC                       = 'MYPARCELBE_CC';
+    public const  CONFIGURATION_CITY                     = 'MYPARCELBE_CITY_TITLE';
+    public const  CONFIGURATION_CLOSED                   = 'MYPARCELBE_CLOSED_TITLE';
+    public const  CONFIGURATION_DELIVERY_CHOICE          = 'MYPARCELBE_DELIVERY_CHOICE_TITLE';
+    public const  CONFIGURATION_DELIVERY_EVENING_TITLE   = 'MYPARCELBE_DELIVERY_EVENING_TITLE';
+    public const  CONFIGURATION_DELIVERY_MORNING_TITLE   = 'MYPARCELBE_DELIVERY_MORNING_TITLE';
+    public const  CONFIGURATION_DELIVERY_STANDARD_TITLE  = 'MYPARCELBE_DELIVERY_STANDARD_TITLE';
+    public const  CONFIGURATION_DELIVERY_TITLE           = 'MYPARCELBE_DELIVERY_TITLE';
+    public const  CONFIGURATION_DISCOUNT                 = 'MYPARCELBE_DISCOUNT_TITLE';
+    public const  CONFIGURATION_FREE                     = 'MYPARCELBE_FREE_TITLE';
+    public const  CONFIGURATION_FROM                     = 'MYPARCELBE_FROM_TITLE';
+    public const  CONFIGURATION_HOUSE_NUMBER             = 'MYPARCELBE_HOUSE_NUMBER_TITLE';
+    public const  CONFIGURATION_LOAD_MORE                = 'MYPARCELBE_LOAD_MORE_TITLE';
+    public const  CONFIGURATION_ONLY_RECIPIENT_TITLE     = 'MYPARCELBE_ONLY_RECIPIENT_TITLE';
+    public const  CONFIGURATION_OPENING_HOURS            = 'MYPARCELBE_OPENING_HOURS_TITLE';
+    public const  CONFIGURATION_PICKUP_LIST_TITLE        = 'MYPARCELBE_PICKUP_LIST_TITLE';
+    public const  CONFIGURATION_PICKUP_MAP_TITLE         = 'MYPARCELBE_PICKUP_MAP_TITLE';
+    public const  CONFIGURATION_PICKUP_TITLE             = 'MYPARCELBE_PICKUP_TITLE';
+    public const  CONFIGURATION_PICK_UP_FROM             = 'MYPARCELBE_PICK_UP_FROM_TITLE';
+    public const  CONFIGURATION_POSTCODE                 = 'MYPARCELBE_POSTCODE_TITLE';
+    public const  CONFIGURATION_RETRY                    = 'MYPARCELBE_RETRY_TITLE';
+    public const  CONFIGURATION_SATURDAY_DELIVERY_TITLE  = 'MYPARCELBE_SATURDAY_DELIVERY_TITLE';
+    public const  CONFIGURATION_SIGNATURE_TITLE          = 'MYPARCELBE_SIGNATURE_TITLE';
+    public const  CONFIGURATION_WRONG_NUMBER_POSTAL_CODE = 'MYPARCELBE_WRONG_NUMBER_POSTAL_CODE';
+    public const  CONFIGURATION_WRONG_POSTAL_CODE_CITY   = 'MYPARCELBE_WRONG_POSTAL_CODE_CITY_TITLE';
+
 
     /**
      * Default data to merge with each config item.
@@ -80,6 +86,66 @@ class CheckoutForm extends AbstractForm
                 'label' => 'Pickup title',
                 'name'  => self::CONFIGURATION_PICKUP_TITLE,
                 'desc'  => 'Title of the pickup option.',
+            ],
+            [
+                'label' => 'House number text',
+                'name'  => self::CONFIGURATION_HOUSE_NUMBER,
+            ],
+            [
+                'label' => 'City text',
+                'name'  => self::CONFIGURATION_CITY,
+            ],
+            [
+                'label' => 'Postal code text',
+                'name'  => self::CONFIGURATION_POSTCODE,
+            ],
+            [
+                'label' => 'Country text',
+                'name'  => self::CONFIGURATION_CC,
+            ],
+            [
+                'label' => 'Opening hours text',
+                'name'  => self::CONFIGURATION_OPENING_HOURS,
+            ],
+            [
+                'label' => 'Load more title',
+                'name'  => self::CONFIGURATION_LOAD_MORE,
+            ],
+            [
+                'label' => 'Pickup map button title',
+                'name'  => self::CONFIGURATION_PICKUP_MAP_TITLE,
+            ],
+            [
+                'label' => 'Pickup list button text',
+                'name'  => self::CONFIGURATION_PICKUP_LIST_TITLE,
+            ],
+            [
+                'label' => 'Retry title',
+                'name'  => self::CONFIGURATION_RETRY,
+            ],
+            [
+                'label' => 'Address not found title',
+                'name'  => self::CONFIGURATION_ADDRESS_NOT_FOUND,
+            ],
+            [
+                'label' => 'Wrong postal code/city combination title',
+                'name'  => self::CONFIGURATION_WRONG_POSTAL_CODE_CITY,
+            ],
+            [
+                'label' => 'Wrong number/postal code title',
+                'name'  => self::CONFIGURATION_WRONG_NUMBER_POSTAL_CODE,
+            ],
+            [
+                'label' => 'From title',
+                'name'  => self::CONFIGURATION_FROM,
+            ],
+            [
+                'label' => 'Free title',
+                'name'  => self::CONFIGURATION_FREE,
+            ],
+            [
+                'label' => 'Discount title',
+                'name'  => self::CONFIGURATION_DISCOUNT,
             ],
         ];
     }
