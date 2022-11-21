89397f64bfd1c9a85a96462d4c018a541d7c38ff
joerivanveen <joeri@myparcel.nl>
2022-11-21 09:07:08 +0100

    fix(shipments): implement feedback

--- src/Provider/OrderLabelProvider.php
+++ src/Provider/OrderLabelProvider.php
@@ -26,7 +26,7 @@ class OrderLabelProvider
 
         if (! empty($labels)) {
             foreach ($labels as &$label) {
-                if ("1" === $label['is_return']) {
+                if ('1' === $label['is_return']) {
                     $label['is_return'] = true;
                     continue;
                 }
