0377082f1a9bfa3c7d0d1b6fbe5fb7cb26d3054c
joerivanveen <joeri@myparcel.nl>
2023-01-03 13:55:02 +0100

    fix: allow upgrade from 1.3.0 without errors

--- upgrade/upgrade-1.3.0.php
+++ upgrade/upgrade-1.3.0.php
@@ -10,11 +10,15 @@ function upgrade_module_1_3_0(MyParcelBE $module): bool
 {
     $deliverySettingsTable = Table::withPrefix(Table::TABLE_DELIVERY_SETTINGS);
 
-    $query = <<<SQL
+    try {
+        $query = <<<SQL
 alter table $deliverySettingsTable add column extra_options text;
 SQL;
 
-    Db::getInstance()->execute($query);
+        Db::getInstance()->execute($query);
+    } catch (\Throwable $e) {
+        // ignore error when column already exists
+    }
 
     return true;
 }
