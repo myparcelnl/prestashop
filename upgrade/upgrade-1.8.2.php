9931fb593a84bc83074e58d340e8db34c0813365
Mark Ernst <33826576+Mark-Ernst@users.noreply.github.com>
2023-02-13 09:14:34 +0100

    Update upgrade/upgrade-1.8.2.php
    Co-authored-by: Joeri van Veen <joeri@myparcel.nl>

--- upgrade/upgrade-1.8.2.php
+++ upgrade/upgrade-1.8.2.php
@@ -5,5 +5,5 @@
  */
 function upgrade_module_1_8_2(MyParcelBE $module): bool
 {
-    return $module->upgrade(Gett\MyparcelBE\Module\Upgrade\Upgrade1_8_::class);
+    return $module->upgrade(Gett\MyparcelBE\Module\Upgrade\Upgrade1_8_2::class);
 }
