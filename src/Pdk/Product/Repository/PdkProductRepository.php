7563fe754e59db963b84563071c70ad4207e8bfe
Edie Lemoine <edie@myparcel.nl>
2023-07-05 16:23:21 +0200

    wip(products): fix product price not being converted to cents

--- src/Pdk/Product/Repository/PdkProductRepository.php
+++ src/Pdk/Product/Repository/PdkProductRepository.php
@@ -8,6 +8,7 @@ use Context;
 use MyParcelNL\Pdk\App\Order\Collection\PdkProductCollection;
 use MyParcelNL\Pdk\App\Order\Model\PdkProduct;
 use MyParcelNL\Pdk\App\Order\Repository\AbstractPdkPdkProductRepository;
+use MyParcelNL\Pdk\Base\Contract\CurrencyServiceInterface;
 use MyParcelNL\Pdk\Facade\Pdk;
 use MyParcelNL\Pdk\Settings\Model\ProductSettings;
 use MyParcelNL\Pdk\Storage\Contract\StorageInterface;
@@ -18,6 +19,16 @@ use Product;
 
 class PdkProductRepository extends AbstractPdkPdkProductRepository
 {
+    /**
+     * @var \MyParcelNL\Pdk\Base\Contract\CurrencyServiceInterface
+     */
+    private $currencyService;
+
+    /**
+     * @var \MyParcelNL\PrestaShop\Repository\PsProductSettingsRepository
+     */
+    private $productSettingsRepository;
+
     /**
      * @var \Doctrine\ORM\EntityRepository
      */
@@ -32,15 +43,18 @@ class PdkProductRepository extends AbstractPdkPdkProductRepository
      * @param  \MyParcelNL\Pdk\Storage\Contract\StorageInterface             $storage
      * @param  \MyParcelNL\PrestaShop\Pdk\Base\Service\PsWeightService       $weightService
      * @param  \MyParcelNL\PrestaShop\Repository\PsProductSettingsRepository $productSettingsRepository
+     * @param  \MyParcelNL\Pdk\Base\Contract\CurrencyServiceInterface        $currencyService
      */
     public function __construct(
         StorageInterface            $storage,
         PsWeightService             $weightService,
-        PsProductSettingsRepository $productSettingsRepository
+        PsProductSettingsRepository $productSettingsRepository,
+        CurrencyServiceInterface    $currencyService
     ) {
         parent::__construct($storage);
         $this->weightService             = $weightService;
         $this->productSettingsRepository = $productSettingsRepository;
+        $this->currencyService           = $currencyService;
 
         /** @var \Doctrine\ORM\EntityManager $entityManager */
         $entityManager             = Pdk::get('ps.entityManager');
@@ -68,7 +82,7 @@ class PdkProductRepository extends AbstractPdkPdkProductRepository
                 'isDeliverable'      => $this->isDeliverable($psProduct),
                 'price'              => [
                     'currency' => Context::getContext()->currency->iso_code,
-                    'amount'   => $psProduct->price,
+                    'amount'   => $this->currencyService->convertToCents($psProduct->price),
                 ],
             ]);
         });
@@ -86,6 +100,7 @@ class PdkProductRepository extends AbstractPdkPdkProductRepository
             $psProductSettings = $this->psProductRepository->findOneBy(['idProduct' => $identifier]);
             $data              = $psProductSettings ? $psProductSettings->toArray() : [];
             $parameters        = json_decode($data['data'] ?? '', true);
+
             return new ProductSettings($parameters);
         });
     }
