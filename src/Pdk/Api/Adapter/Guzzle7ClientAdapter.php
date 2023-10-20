8fcc20b0691ba7b7886c9b6a14428fc3fc6af97a
Edie Lemoine <edie@myparcel.nl>
2023-10-20 11:36:05 +0200

    refactor: pull guzzle client into the project

--- src/Pdk/Api/Adapter/Guzzle7ClientAdapter.php
+++ src/Pdk/Api/Adapter/Guzzle7ClientAdapter.php
@@ -4,4 +4,53 @@ declare(strict_types=1);
 
 namespace MyParcelNL\PrestaShop\Pdk\Api\Adapter;
 
-final class Guzzle7ClientAdapter extends \MyParcelNL\Pdk\Tests\Api\Guzzle7ClientAdapter { }
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+use MyParcelNL\Pdk\Api\Contract\ClientAdapterInterface;
+use MyParcelNL\Pdk\Api\Contract\ClientResponseInterface;
+use MyParcelNL\Pdk\Api\Response\ClientResponse;
+
+final class Guzzle7ClientAdapter implements ClientAdapterInterface
+{
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    private $client;
+
+    /**
+     * @param  \GuzzleHttp\Client $client
+     */
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @param  string $httpMethod
+     * @param  string $uri
+     * @param  array  $options
+     *
+     * @return \MyParcelNL\Pdk\Api\Contract\ClientResponseInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function doRequest(string $httpMethod, string $uri, array $options = []): ClientResponseInterface
+    {
+        $method = strtolower($httpMethod);
+
+        $requestOptions = array_filter([
+            RequestOptions::HEADERS => $options['headers'] ?? null,
+            RequestOptions::BODY    => $options['body'] ?? null,
+        ]);
+
+        $requestOptions[RequestOptions::HTTP_ERRORS] = false;
+
+        $response     = $this->client->request($method, $uri, $requestOptions);
+        $responseBody = $response->getBody();
+
+        $body = $responseBody->isReadable()
+            ? $responseBody->getContents()
+            : null;
+
+        return new ClientResponse($body, $response->getStatusCode());
+    }
+}
