d665b27e970941ae746da7a53e919103f2cadf09
Edie Lemoine <edie@myparcel.nl>
2023-10-23 15:03:14 +0200

    test: update entry test

--- tests/Unit/EntryTest.php
+++ tests/Unit/EntryTest.php
@@ -5,11 +5,28 @@ declare(strict_types=1);
 
 namespace MyParcelNL\PrestaShop;
 
+use MyParcelNL\Pdk\Facade\Pdk;
 use MyParcelNL\PrestaShop\Tests\Uses\UsesMockPlugin;
+use Throwable;
 use function MyParcelNL\Pdk\Tests\usesShared;
+use function Spatie\Snapshots\assertMatchesJsonSnapshot;
 
 usesShared(new UsesMockPlugin());
 
 it('boots plugin', function () {
     expect(true)->toBeTrue();
+});
+
+it('has hooks', function () {
+    assertMatchesJsonSnapshot(json_encode(Pdk::get('moduleHooks')));
+});
+
+it('installs and uninstalls module', function () {
+    /** @var \MyParcelNL $module */
+    $module = Pdk::get('moduleInstance');
+
+    expect($module->install())
+        ->not->toThrow(Throwable::class)
+        ->and($module->uninstall())
+        ->not->toThrow(Throwable::class);
 });
