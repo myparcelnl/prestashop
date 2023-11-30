fbbc6114acfdbfd1d46a82e5946d17823cdd92fc
Edie Lemoine <edie@myparcel.nl>
2023-11-30 13:35:05 +0100

    fix(database): fix creating indexes on certain db drivers

--- src/Database/Sql/CreateIndexSqlBuilder.php
+++ src/Database/Sql/CreateIndexSqlBuilder.php
@@ -19,7 +19,7 @@ final class CreateIndexSqlBuilder extends SqlBuilder
     public function build(): string
     {
         return sprintf(
-            'CREATE INDEX IF NOT EXISTS `%s` ON `%s` (%s);',
+            'CREATE INDEX `%s` ON `%s` (%s);',
             $this->index,
             $this->getTable(),
             implode(', ', $this->columns)
