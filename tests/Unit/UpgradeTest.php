7a5b0674059aa90d8de76652329d9085178bc1f3
Edie Lemoine <edie@myparcel.nl>
2024-04-24 13:55:22 +0200

    test: add unhappy flow test case

--- tests/Unit/UpgradeTest.php
+++ tests/Unit/UpgradeTest.php
@@ -13,21 +13,33 @@ use MyParcelNL\Pdk\Settings\Contract\PdkSettingsRepositoryInterface;
 use MyParcelNL\PrestaShop\Tests\Mock\MockMyParcelNL;
 use MyParcelNL\PrestaShop\Tests\Mock\MockPsModule;
 use MyParcelNL\PrestaShop\Tests\Uses\UsesMockPsPdkInstance;
+use Psr\Log\LoggerInterface;
 use function expect;
 use function MyParcelNL\Pdk\Tests\usesShared;
 
 usesShared(new UsesMockPsPdkInstance());
 
-function runUpgrade(string $newVersion): void
+function runUpgradeSuccessfully(string $newVersion): void
 {
-    require_once sprintf('%s/../../upgrade/upgrade-%s.php', __DIR__, $newVersion);
     /** @var \MyParcelNL\Pdk\Tests\Bootstrap\MockSettingsRepository $settingsRepository */
     $settingsRepository = Pdk::get(PdkSettingsRepositoryInterface::class);
+
+    $result = runUpgrade($newVersion);
+
+    expect($settingsRepository->get(Pdk::get('settingKeyInstalledVersion')))
+        ->toBe($newVersion)
+        ->and($result)
+        ->toBeTrue();
+}
+
+function runUpgrade(string $newVersion): bool
+{
+    require_once sprintf('%s/../../upgrade/upgrade-%s.php', __DIR__, $newVersion);
 
     /** @var \MyParcelNL\Pdk\Tests\Bootstrap\MockPdk $pdk */
     $pdk = Pdk::get(PdkInterface::class);
     /**
-     * @TODO: App info is currently set hard coded in the pdk and can't be overridden.
+     * @TODO: App info is currently set hard coded in the pdk and is not overridden during initialisation.
      */
     $pdk->set('appInfo', new AppInfo(['version' => $newVersion]));
 
@@ -36,12 +48,8 @@ function runUpgrade(string $newVersion): void
     MockPsModule::setInstance($moduleInstance->name, $moduleInstance);
 
     $replacedVersion = str_replace('.', '_', $newVersion);
-    $result          = call_user_func("upgrade_module_$replacedVersion", $moduleInstance);
 
-    expect($settingsRepository->get(Pdk::get('settingKeyInstalledVersion')))
-        ->toBe($newVersion)
-        ->and($result)
-        ->toBeTrue();
+    return call_user_func("upgrade_module_$replacedVersion", $moduleInstance);
 }
 
 it('runs upgrade with previous version saved', function (string $previousVersion, string $newVersion) {
@@ -49,7 +57,7 @@ it('runs upgrade with previous version saved', function (string $previousVersion
     $settingsRepository = Pdk::get(PdkSettingsRepositoryInterface::class);
     $settingsRepository->store(Pdk::get('settingKeyInstalledVersion'), $previousVersion);
 
-    runUpgrade($newVersion);
+    runUpgradeSuccessfully($newVersion);
 })->with([
     ['3.0.0', '4.0.0'],
 ]);
@@ -59,7 +67,20 @@ it('runs upgrade with old api key saved', function (string $previousVersion, str
     $settingsRepository = Pdk::get(PdkSettingsRepositoryInterface::class);
     $settingsRepository->store('MYPARCELNL_API_KEY', $previousVersion);
 
-    runUpgrade($newVersion);
+    runUpgradeSuccessfully($newVersion);
 })->with([
     ['3.0.0', '4.0.0'],
 ]);
+
+it('catches errors on upgrade', function () {
+    /** @var \MyParcelNL\Pdk\Tests\Bootstrap\MockLogger $logger */
+    $logger = Pdk::get(LoggerInterface::class);
+
+    expect(runUpgrade('4.0.0'))
+        ->toBeFalse()
+        ->and($logger->getLogs()[0])
+        ->toHaveKeysAndValues([
+            'level'   => 'error',
+            'message' => '[PDK]: Failed to install module',
+        ]);
+});
