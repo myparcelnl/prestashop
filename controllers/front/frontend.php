da56fab01599c3db917760443d127fef8ffc3179
jochemgravendeel <jochem.gravendeel@myparcel.nl>
2026-05-13 11:19:58 +0200

    feat: add frontend module front controller for public capabilities proxy endpoint

---
+++ controllers/front/frontend.php
@@ -0,0 +1,46 @@
+<?php
+/** @noinspection AutoloadingIssuesInspection,PhpUnused */
+
+declare(strict_types=1);
+
+use MyParcelNL\Pdk\App\Api\PdkEndpoint;
+use MyParcelNL\Pdk\Facade\Logger;
+use MyParcelNL\Pdk\Facade\Pdk;
+use MyParcelNL\PrestaShop\Facade\EntityManager;
+use MyParcelNL\PrestaShop\Facade\MyParcelModule;
+use Symfony\Component\HttpFoundation\Request;
+
+if (! defined('_PS_VERSION_')) {
+    return;
+}
+
+class MyParcelNLFrontendModuleFrontController extends ModuleFrontController
+{
+    public function initContent(): void
+    {
+        if (! MyParcelModule::isEnabled()) {
+            $this->sendResponse(400, 'Module is not enabled');
+        }
+
+        try {
+            $request  = Request::createFromGlobals();
+            $endpoint = Pdk::get(PdkEndpoint::class);
+            $response = $endpoint->call($request, PdkEndpoint::CONTEXT_FRONTEND);
+        } catch (Throwable $e) {
+            Logger::error($e->getMessage(), ['values' => $_REQUEST]);
+            $this->sendResponse(400, $e->getMessage());
+        }
+
+        EntityManager::flush();
+        $response->send();
+        die(1);
+    }
+
+    private function sendResponse(int $statusCode, string $message): void
+    {
+        (new \Symfony\Component\HttpFoundation\Response($message))
+            ->setStatusCode($statusCode)
+            ->send();
+        die(1);
+    }
+}
